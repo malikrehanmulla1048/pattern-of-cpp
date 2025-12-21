40f88ca9b1162aded5ed65ab329bacb44395baf1
malikrehanmulla1048 <malikrehanmulla107@gmail.com>
2025-12-21 17:48:40 +0530

    container with most water

---
+++ most_water.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+int most_water_brute_force(const vector<int>& height)
+{
+    int max_height=INT_MAX;
+    int max_area=INT_MIN;
+    int n=height.size();
+    int min_height=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        int width=0;
+        for(int j=i;j<n;j++)
+        {
+            min_height=min(height[i],height[j]);
+            max_area=max(max_area,min_height*width);
+            width++;
+        }
+    }
+    return max_area;
+}
+int main()
+{
+    vector<int> height={1,8,6,2,5,4,8,3,7,9};
+    int max_area=most_water_brute_force(height);
+    cout<<"the maximum area of water that can be contained is: "<<max_area<<endl;
+    return 0;
+}
